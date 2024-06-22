0f90dce4ee645c29ed49809b50b86f721c60ad5f
elforgaani <mohamed.elforgani1999@gmail.com>
2024-06-22 22:20:01 +0200

    Added user module routes

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -1,6 +1,14 @@
 import { Router } from "express";
 import * as UsersController from "./users.controller.js";
+import { signUpRequestInputVaildator } from "../../middlewares/validators.middleware.js";
 
 const router = Router();
+
+router.post("/sign-up", signUpRequestInputVaildator, UsersController.signUp);
+router.post("/sign-in", UsersController.signIn);
+router.get("/:id", UsersController.getSpecificUser);
+router.get("/", UsersController.getAllUsers);
+router.put("/", UsersController.updateUser);
+router.delete("/", UsersController.deleteUser);
 
 export default router;
