7bf59fd1f31e026a073ddd767e6e8425ae7970da
elforgaani <mohamed.elforgani1999@gmail.com>
2024-06-22 22:19:52 +0200

    Added UserController functions

---
+++ src/modules/users/users.controller.js
@@ -0,0 +1,11 @@
+export const signUp = async (req, res) => {};
+
+export const signIn = async (req, res) => {};
+
+export const getSpecificUser = async (req, res) => {};
+
+export const getAllUsers = async (req, res) => {};
+
+export const updateUser = async (req, res) => {};
+
+export const deleteUser = async (req, res) => {};
