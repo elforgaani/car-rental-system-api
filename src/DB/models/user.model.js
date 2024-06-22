4427537cc9e7ad676cd70537c1190541ee43c3e5
elforgaani <mohamed.elforgani1999@gmail.com>
2024-06-22 22:19:23 +0200

    Added user model!

--- src/DB/models/user.model.js
+++ src/DB/models/user.model.js
@@ -2,4 +2,30 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const shcema = new Schema({}, { timestamps: true  });
+const shcema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      uniqe: true,
+    },
+    phone_number: {
+      type: String,
+      required: true,
+      uniqe: true,
+      minLength: 10,
+      maxLength: 10,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || model("User", shcema);
