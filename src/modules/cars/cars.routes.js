c00b44b9e3a49f5c66d0f832aa291faf452c8693
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-06-23 15:54:20 +0200

    Added CarsRoutes!

--- src/modules/cars/cars.routes.js
+++ src/modules/cars/cars.routes.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
 import * as CarsController from "./cars.controller.js";
+import { authMiddleware } from '../../middlewares/auth.middleware.js'
+import { addCarRequestInputVlidationMiddleware } from "../../middlewares/validators.middleware.js";
 const router = Router();
+
+router.post('', authMiddleware, addCarRequestInputVlidationMiddleware, CarsController.addCar);
+router.get('/:id', authMiddleware, CarsController.getSpecificCar);
+router.get('/', authMiddleware, CarsController.getAllCars);
+router.put('/:id', authMiddleware, CarsController.updateCar);
+router.delete(':id', authMiddleware, CarsController.deleteCar);
+
 
 export default router;
