57e1c119aeaaccd4eb73074f1c0d08230b60a1b5
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-06-24 21:39:30 +0200

    Added Rental Routes

--- src/modules/rentals/rentals.routes.js
+++ src/modules/rentals/rentals.routes.js
@@ -1,6 +1,14 @@
 import { Router } from "express";
 import * as RentalsController from "./rentals.controller.js";
-
+import { authMiddleware } from '../../middlewares/auth.middleware.js'
+import { createRentalInputValidationMiddleware } from '../../middlewares/validators.middleware.js';
 const router = Router();
+
+router.post('/', authMiddleware, createRentalInputValidationMiddleware, RentalsController.createRental);
+router.put('/:id', authMiddleware, RentalsController.updateRental);
+router.delete('/:id', authMiddleware, RentalsController.deleteRental)
+router.get('/', authMiddleware, RentalsController.getAllrentals)
+router.get('/:id', authMiddleware, RentalsController.getSpecificRental)
+
 
 export default router;
