b966a1379c102b73e37ebfcc8d7c962a80f36434
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-06-26 11:56:40 +0200

    Added new Car Routes

--- src/modules/cars/cars.routes.js
+++ src/modules/cars/cars.routes.js
@@ -4,11 +4,20 @@ import { authMiddleware } from '../../middlewares/auth.middleware.js'
 import { addCarRequestInputVlidationMiddleware } from "../../middlewares/validators.middleware.js";
 const router = Router();
 
-router.post('', authMiddleware, addCarRequestInputVlidationMiddleware, CarsController.addCar);
+router.post('', authMiddleware, addCarRequestInputVlidationMiddleware
+    , CarsController.addCar);
+router.get('/get-cars-based-on-model', authMiddleware, CarsController.getCarsBasedOnModel);
+router.get('/get-available-cars', authMiddleware, CarsController.getAvailableCars);
+router.get('/rented-or-by-name', authMiddleware, CarsController.rentedOrByName);
+router.get('/available-or-rented', authMiddleware, CarsController.availableOrRented)
+
+
 router.get('/:id', authMiddleware, CarsController.getSpecificCar);
 router.get('/', authMiddleware, CarsController.getAllCars);
 router.put('/:id', authMiddleware, CarsController.updateCar);
 router.delete('/:id', authMiddleware, CarsController.deleteCar);
+
+
 
 
 export default router;
